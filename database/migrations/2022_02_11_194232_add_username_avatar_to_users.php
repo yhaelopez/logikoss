5596c702085ef10254151be47b788662359736a3
Yhael <yhaelopez@gmail.com>
2022-02-11 13:12:16 -0700

    spatie media library set up complete

--- database/migrations/2022_02_11_194232_add_username_avatar_to_users.php
+++ database/migrations/2022_02_11_194232_add_username_avatar_to_users.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->after('email', function($table) {
+                $table->string('username')->unique();
+            });
         });
     }
 
@@ -26,7 +28,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['username']);
         });
     }
 };
